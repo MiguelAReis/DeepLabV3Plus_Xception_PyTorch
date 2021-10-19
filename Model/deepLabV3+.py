21146310d1230767587f5a8a60ecf5fdb3741d38
Miguel Reis <miguelareis98@gmail.com>
2021-10-19 15:00:58 +0100

    Beginning

---
+++ Model/deepLabV3+.py
@@ -0,0 +1,21 @@
+import math
+import torch
+import torch.nn as nn
+
+class CNNModel(nn.Module)
+
+	def __init__(self):
+		super(CNNModel,self).__init()
+
+
+		#define Model
+
+
+
+	def forward(self):
+
+
+		#define forward function
+
+
+		
